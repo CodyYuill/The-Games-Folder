94707a59bbb7cfc90f1f13415378ee95f7e3a1bf
Cody Yuill <codyyuill@hotmail.com>
2021-03-03 16:49:05 -0500

    setup project file structure, server is setup with no routes

---
+++ models/user.js
@@ -0,0 +1,8 @@
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+
+  });
+
+  return User;
+};
---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+// Requiring our models and passport as we've configured it
+const db = require("../models");
+
+module.exports = function(app) {
+
+  app.post("/api/", (req, res) => {
+
+  });
+
+  app.get("/api/", (req, res) => {
+
+  });
+};
