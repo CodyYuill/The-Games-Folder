a8ee724730edb9d6c0927f3533c7290d4a8dd59d
Cody Yuill <codyyuill@hotmail.com>
2021-03-06 11:17:17 -0500

    added findall games route, and findOne game based off of slug route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,11 +3,25 @@ const db = require("../models");
 
 module.exports = function(app) {
 
-    app.post("/api/", (req, res) => {
-
+    app.get("/api/all-games", (req, res) => {
+        db.Game.findAll({}).then(function(result){
+            res.json(result);
+        });
     });
 
-    app.get("/api/", (req, res) => {
+    app.get("/api/:game", (req, res) => {
+        db.Game.findOne({
+            where:{
+                slug: req.params.game
+            }
+            //include: [db.reviews] if we get there
+        }).then(function(result){
+            //grab rest of info thats we arent storing in the database from RAWG
+            res.json(result);
+        });
+    });
+
+    app.post("/api/", (req, res) => {
 
     });
 };
