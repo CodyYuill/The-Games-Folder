63a649478dd3dd0365962f7c903c97bdeaea70ac
Cody Yuill <codyyuill@hotmail.com>
2021-03-06 10:36:10 -0500

    fixed server.js to sync sequelize models to our databse, setup games model

--- models/games.js
+++ models/games.js
@@ -1,8 +1,18 @@
 // Creating our Game model
 module.exports = function(sequelize, DataTypes) {
     const Game = sequelize.define("Game", {
-  
+        name:{
+            type: DataTypes.STRING
+        },
+        slug:{
+            type: DataTypes.STRING
+        },
+        genres:{
+            type: DataTypes.STRING
+        },
+        inventory:{
+            type: DataTypes.INTEGER
+        }
     });
-  
     return Game;
-  };
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,3 @@
-
-
 module.exports = function(app) {
     app.get("/", function(req, res){
         res.render("filetoname");
