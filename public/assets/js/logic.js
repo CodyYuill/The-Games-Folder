f2e08bdf763e06ac34149ac834d581068d324582
Michael Wall <mikesterwallace8@gmail.com>
2021-03-08 19:09:19 -0500

    esling error fix

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -3,33 +3,32 @@ $(document).ready(() => {
 
 });
 
-let searchBtn = document.getElementById('searchBtn');
+let searchBtn = document.getElementById("searchBtn");
 
 // search input field
-let gameSearch = document.getElementById('gameSearch').value;
+let gameSearch = document.getElementById("gameSearch").value;
 
 // gameIMg
-let gameImg = document.getElementById('gameImg');
+let gameImg = document.getElementById("gameImg");
 
 // API url 
-let url = "wwww.gamesdgsdgsg.com/api/" + consoleSearch + "/" + gameSearch
+let url = `wwww.gamesdgsdgsg.com/api/${consoleSearch}/${gameSearch}`;
 
 searchBtn.addEventListener(click, searchGame);
 
 function searchName () {
     
     fetch(url)
-    .then(function (result) {
-    return result.json()
-    })
-    .then(function (gameData) {
+        .then(function (result) {
+            return result.json()
+    }).then(function (gameData) {
 
         let gameImgDB = JSON.stringify(gameData.image);
 
-        gameImg.setAttribute("src", gameImgDB)
+        gameImg.setAttribute("src", gameImgDB);
 
-         document.getElementById('day1H').textContent
-    })
+        document.getElementById("day1H").textContent;
+    });
 }
 
 
