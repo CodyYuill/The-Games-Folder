992bdfa0e861c8a51fec2e7c8bab1a2b5aa5b8fe
Michael Wall <mikesterwallace8@gmail.com>
2021-03-08 19:12:37 -0500

    comment api routes to avoid conflict

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,34 +1,34 @@
-$(document).ready(() => {
+// $(document).ready(() => {
 
 
-});
+// });
 
-let searchBtn = document.getElementById("searchBtn");
+// let searchBtn = document.getElementById("searchBtn");
 
-// search input field
-let gameSearch = document.getElementById("gameSearch").value;
+// // search input field
+// let gameSearch = document.getElementById("gameSearch").value;
 
-// gameIMg
-let gameImg = document.getElementById("gameImg");
+// // gameIMg
+// let gameImg = document.getElementById("gameImg");
 
-// API url 
-let url = `wwww.gamesdgsdgsg.com/api/${consoleSearch}/${gameSearch}`;
+// // API url 
+// let url = `wwww.gamesdgsdgsg.com/api/${consoleSearch}/${gameSearch}`;
 
-searchBtn.addEventListener(click, searchGame);
+// searchBtn.addEventListener(click, searchGame);
 
-function searchName () {
+// function searchName () {
     
-    fetch(url)
-        .then(function (result) {
-            return result.json()
-    }).then(function (gameData) {
+//     fetch(url)
+//         .then(function (result) {
+//             return result.json()
+//     }).then(function (gameData) {
 
-        let gameImgDB = JSON.stringify(gameData.image);
+//         let gameImgDB = JSON.stringify(gameData.image);
 
-        gameImg.setAttribute("src", gameImgDB);
+//         gameImg.setAttribute("src", gameImgDB);
 
-        document.getElementById("day1H").textContent;
-    });
-}
+//         document.getElementById("day1H").textContent;
+//     });
+// }
 
 
